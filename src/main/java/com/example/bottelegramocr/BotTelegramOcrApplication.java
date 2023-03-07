fd5aa3dff399d50c5dfce8c6835f30f3abf318d5
Macailh <salvadorenriquegb@gmail.com>
2023-03-07 07:53:13 -0600

    Add main class

---
+++ src/main/java/com/example/bottelegramocr/BotTelegramOcrApplication.java
@@ -0,0 +1,13 @@
+package com.example.bottelegramocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotTelegramOcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotTelegramOcrApplication.class, args);
+    }
+
+}
