5437d4d7c819f7d7233cdf3f80fbdcd157c02cd1
Macailh <salvadorenriquegb@gmail.com>
2023-03-07 07:51:35 -0600

    Initial commit

---
+++ src/test/java/com/example/bottelegramocr/BotTelegramOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bottelegramocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotTelegramOcrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
